81ff1fb6dee9ca705ec2ebb279fa18d40d3237b8
filipthedeveloper <filip.bakidis@gmail.com>
2020-11-15 11:19:33 +0100

    6. Styling

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
 body{
     background:#1b1b1b;
+    font-family:"Inter", sans-serif;
 }
 button{
     font-weight:bold;
@@ -19,6 +20,7 @@ button{
     background:transparent;
     color:white;
     transition:all 0.5s ease;
+    font-family:"Inter", sans-serif;
     &:hover{
         background-color:#23d997;
         color:white;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -4,50 +4,81 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+import { About, Description, Image } from "../styles";
+import styled from "styled-components";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span>services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img alt="icon" src={clock} alt="" />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img alt="icon" src={teamwork} alt="" />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img alt="icon" src={diaphragm} alt="" />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img alt="icon" src={money} alt="" />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <div className="image">
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img alt="icon" src={home2} alt="" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
